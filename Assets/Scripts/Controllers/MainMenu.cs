ccde9278177b3829293e8acce01366532f9b13a2
Yurii Tsybarev <arhynexus-wrath@mail.ru>
2023-03-19 22:17:56 +0300

    Changed main menuscript
    Now we can choose start new game or resume previous saved game.

--- Assets/Scripts/Controllers/MainMenu.cs
+++ Assets/Scripts/Controllers/MainMenu.cs
@@ -13,11 +13,29 @@ namespace TowerDefenceClone
         private void Start()
         {
             m_ContinueButton.interactable = FileHandler.HasFile(MapCompletion.filename);
+            m_ConfirmPanel.SetActive(false);
         }
         public void OnButtonNewGame()
         {
+            if(FileHandler.HasFile(MapCompletion.filename))
+            {
+                m_ConfirmPanel.SetActive(true);
+            }
+            else
+            {
+                FileHandler.Reset(MapCompletion.filename);
+                SceneManager.LoadScene(1);
+            }
+        }
+
+        public void OnButtonYesNewGame()
+        {
             FileHandler.Reset(MapCompletion.filename);
             SceneManager.LoadScene(1);
+        }
+        public void OnButtonNoNewGame()
+        {
+            m_ConfirmPanel.SetActive(false);
         }
 
         public void OnButtonResumeGame()
