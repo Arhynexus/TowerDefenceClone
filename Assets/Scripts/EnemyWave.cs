7312295250593790189e6d0ff08f1cfc662b8d41
Yurii Tsybarev <arhynexus-wrath@mail.ru>
2023-03-23 15:31:01 +0300

    Added Waves mechanic
    now we can spawn waves of enemies

---
+++ Assets/Scripts/EnemyWave.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefenceClone
+{
+    public class EnemyWave: MonoBehaviour
+    {
+        [Serializable]
+        private class Squad
+        {
+            public EnemyAsset asset;
+            public int count;
+        }
+
+        [Serializable]
+        private class PathGroup
+        {
+            public Squad[] squads;
+        }
+
+        [SerializeField] private PathGroup[] groups;
+        [SerializeField] private float m_PrepareTime = 10f;
+
+        private event Action OnWaveReady;
+
+        private void Awake()
+        {
+            enabled = false;
+        }
+        public IEnumerable<(EnemyAsset asset, int count, int pathIndex)> EnumerateSquads()
+        {
+            for (int i = 0; i < groups.Length; i++)
+            {
+                foreach (var squad in groups[i].squads)
+                {
+                    yield return (squad.asset, squad.count, i);
+                }
+            }
+        }
+
+        public void Prepare(Action spawnEnemies)
+        {
+            m_PrepareTime += Time.time;
+            enabled = true;
+            OnWaveReady += spawnEnemies;
+        }
+
+        [SerializeField] private EnemyWave m_NextWave;
+        public EnemyWave PrepareNext(Action spawnEnemies)
+        {
+            OnWaveReady -= spawnEnemies;
+            if (m_NextWave) m_NextWave.Prepare(spawnEnemies);
+            return m_NextWave;
+        }
+
+        private void Update()
+        {
+            if (Time.time >= m_PrepareTime)
+            {
+                enabled = false;
+                OnWaveReady.Invoke();
+            }
+        }
+    }
+}
--- Assets/Scripts/EnemyWaveManager.cs
+++ Assets/Scripts/EnemyWaveManager.cs
@@ -3,24 +3,37 @@ using UnityEngine;
 namespace TowerDefenceClone
 {
 
-
     public class EnemyWaveManager : MonoBehaviour
     {
         [SerializeField] private Path[] m_Paths;
         [SerializeField] private EnemyWave m_CurrentWave;
+        [SerializeField] private Enemy m_EnemyPrefab;
 
         private void Start()
         {
-            m_CurrentWave.OnWaveReady += SpawnEnemies;
-            m_CurrentWave.Prepare();
+            m_CurrentWave.Prepare(SpawnEnemies);
         }
 
         private void SpawnEnemies()
         {
-            m_CurrentWave.OnWaveReady -= SpawnEnemies;
-            m_CurrentWave = m_CurrentWave.Next();
-            m_CurrentWave.OnWaveReady += SpawnEnemies;
-            m_CurrentWave.Prepare();
+            foreach ((EnemyAsset asset, int count, int pathIndex) in m_CurrentWave.EnumerateSquads())
+            {
+                if(pathIndex < m_Paths.Length)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        var e = Instantiate(m_EnemyPrefab, m_Paths[pathIndex].StartArea.GetRandomINsideZone(), Quaternion.identity);
+                        e.Use(asset);
+                        e.GetComponent<TDPatrolController>().SetPath(m_Paths[pathIndex]);
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning($"Invalid pathindex in {name}");
+                }
+            }
+            
+            m_CurrentWave = m_CurrentWave.PrepareNext(SpawnEnemies);
         }
     }
 }
